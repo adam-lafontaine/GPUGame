c147c1a727f74f883abfd0ae9fd9e34b6a44bfa4
adam-lafontaine <adam.lafontaine@gmail.com>
2022-06-27 11:07:06 -0400

    new defines

--- src/device/cuda_def.cuh
+++ src/device/cuda_def.cuh
@@ -1,5 +1,9 @@
 #pragma once
 
+#define CUDA_NOT_INSTALLED
+
+#ifndef CUDA_NOT_INSTALLED
+
 #include <cuda_runtime.h>
 
 #define GPU_KERNAL __global__
@@ -11,6 +15,22 @@
 #define GPU_UNIFIED __device__ __managed__
 #define GPU_CONSTEXPR_FUNCTION __device__ constexpr
 
+#define cuda_barrier __syncthreads
+
+#else
+
+#define GPU_KERNAL /*__global__*/
+#define GPU_FUNCTION /*__device__*/
+#define HOST_FUNCTION /*__host__*/
+#define GPU_GLOBAL_VARIABLE /*__device__*/
+#define GPU_GLOBAL_CONSTANT /*__constant__*/
+#define GPU_BLOCK_VARIABLE /*__shared__*/
+#define GPU_UNIFIED /*__device__ __managed__*/
+#define GPU_CONSTEXPR_FUNCTION /*__device__ constexpr*/
+
+inline void cuda_barrier(){}
+
+#endif
+
 #define CUDA_PRINT_ERROR
 
-#define cuda_barrier __syncthreads
